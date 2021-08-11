2bb25baf6d97085a41c36038187bc2d5b696cc78
Paulo Henerique <php_paulo@hotmail.com>
2021-08-11 11:24:28 -0300

    change url api db

--- src/services/bdBo/CadastrarCotacao.js
+++ src/services/bdBo/CadastrarCotacao.js
@@ -7,7 +7,7 @@ const headers = {
 
 const apiCheckout = axios.create({
   baseURL:
-    "https://cors-anywhere.herokuapp.com/https://api-banco-dados-dot-bidu-digital-dev.appspot.com/auto/",
+    "https://cors-anywhere.herokuapp.com/https://api-banco-dados-w6uut5cujq-rj.a.run.app/auto/",
 });
 
 export const CadastrarCotacaoBd = (quote, body) =>
